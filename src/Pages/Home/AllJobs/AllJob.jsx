1f73821572769d7aec2663db70e0acfd15583942
MujahidulIslam4541 <mirifat4541@gmail.com>
2024-12-27 13:01:56 +0600

    added buttons and active

--- src/Pages/Home/AllJobs/AllJob.jsx
+++ src/Pages/Home/AllJobs/AllJob.jsx
@@ -16,39 +16,47 @@ const AllJob = ({ job }) => {
   } = job;
   return (
     <div>
-      <div className="card card-compact hover:border hover:border-blue-300 hover:-inset-y-1 shadow-xl transition ease-in duration-500">
-        <div className="flex items-center gap-4 mt-2">
+      <div className="card card-compact h-[450px]  bg-white border border-gray-200 shadow-md hover:shadow-lg transition-transform transform hover:-translate-y-1 duration-500 group">
+        <div className="flex items-center gap-3 p-3">
           <figure>
-            <img className="w-12" src={company_logo} alt="Shoes" />
+            <img
+              className="w-12 rounded-full"
+              src={company_logo}
+              alt="Company Logo"
+            />
           </figure>
           <div>
-            <h2 className="text-2xl">{company}</h2>
-            <p className="flex items-center gap-1">
-              <FaLocationDot />
+            <h2 className="text-xl font-semibold text-gray-800">{company}</h2>
+            <p className="flex items-center gap-1 text-gray-600">
+              <FaLocationDot className="text-blue-500" />
               {location}
             </p>
           </div>
         </div>
-        <div className="card-body">
-          <h2 className="card-title">
+        <div className="card-body p-3">
+          <h2 className="card-title flex items-center">
             {title}
-            <div className="badge badge-primary">NEW</div>
+            <div className="badge badge-primary ml-2">NEW</div>
           </h2>
-          <p>{jobType}</p>
-          <p>{description}</p>
-
-          <div className="grid grid-cols-3">
+          <p className="text-gray-700">{jobType}</p>
+          <p className="text-gray-600">{description}</p>
+          <div className="grid grid-cols-3 gap-2 my-2">
             {requirements?.map((requirement, index) => (
-              <p key={index}>{requirement}</p>
+              <p
+                key={index}
+                className="text-sm text-gray-600 bg-gray-100 px-2 py-1 rounded-md shadow-sm"
+              >
+                {requirement}
+              </p>
             ))}
           </div>
-          <div className="card-actions ">
-            <p>
-              Salary: {salaryRange?.min}-{salaryRange?.max}
+          <div className="card-actions flex justify-between items-center">
+            <p className="text-gray-800 font-medium">
+              Salary: {salaryRange?.min}-{salaryRange?.max}{" "}
               {salaryRange?.currency}
             </p>
             <Link to={`jobs/${_id}`}>
-              <button className="btn hover:bg-blue-500 hover:text-white">
+              <button className="btn bg-gray-500 text-white group-hover:bg-blue-500">
                 Apply now
               </button>
             </Link>
--- src/Pages/Home/AllJobs/AllJobs.jsx
+++ src/Pages/Home/AllJobs/AllJobs.jsx
@@ -1,15 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 import AllJob from "./AllJob";
 import { FaBriefcase } from "react-icons/fa";
+import { useState } from "react";
 
 const AllJobs = () => {
   const AllJobs = useLoaderData();
+  const [activeButton, setActiveButton] = useState(null);
+
+  const buttons = [
+    "Management",
+    "Marketing & Sale",
+    "Finance",
+    "Human Resource",
+    "Content Writer",
+    "Retail & Product",
+  ];
   return (
     <>
       <div className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white p-16 rounded-lg shadow-lg">
         <div className="flex flex-col md:flex-row items-center gap-8">
           <div className="text-6xl md:text-8xl text-yellow-400">
-            <FaBriefcase /> 
+            <FaBriefcase />
           </div>
 
           {/* Text Section */}
@@ -24,6 +35,24 @@ const AllJobs = () => {
             </p>
           </div>
         </div>
+      </div>
+
+      <div className=" mt-10 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-3">
+        {buttons.map((button, index) => (
+          <button
+            key={index}
+            onClick={() => setActiveButton(index)}
+            className={`py-2 px-4 rounded-md hover:border-blue-500 hover:text-blue-500 transition-colors duration-300
+            ${
+              activeButton === index
+                ? "border-2 border-blue-500 text-blue-500"
+                : "border border-gray-200"
+            } 
+            `}
+          >
+            {button}
+          </button>
+        ))}
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-10">
